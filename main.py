defcf258478e249c8e159eb6c27c2794944a2e84
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-08-17 02:00:01 +0300

    Trying JSON output

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import traceback
-
+import json
 
 def searchApi(query):
     endpoint = "http://prod.media.jio.com/apis/common/v3.1/search/auto"
@@ -8,12 +8,23 @@ def searchApi(query):
         "q": query
     }
     try:
+        list_msg = []
         response = requests.post(endpoint, data=data)
         if(response.status_code == 200):
-            for msg in response:
-                print(msg)
+            # for msg in response:
+            #     print(msg)
+            return response
     except Exception:
         print(traceback.format_exc())
 
 
-searchApi("avengers")
+text_msg = searchApi('avengers')
+print(text_msg)
+for msg in text_msg:
+    print(msg)
+
+#
+# your_json = f"{searchApi('avengers')}"
+# parsed = json.loads(your_json)
+# print(json.dumps(parsed, indent=4, sort_keys=True))
+
